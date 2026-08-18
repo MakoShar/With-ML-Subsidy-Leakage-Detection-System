9277a0bbe8fc57c1700cf6c36bba58b06ad60642
Anoop Verma <77383931+MakoShar@users.noreply.github.com>
2026-08-18 07:53:54 +0530

    prep

--- app.py
+++ app.py
@@ -595,4 +595,5 @@ def slds_page():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
